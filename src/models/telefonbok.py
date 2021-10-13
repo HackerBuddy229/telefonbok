5b5531b7eb1479b521079968e1ea475cac98fb8e
Rasmus Bengtsson <rasmus@beryllium-tech.eu>
2021-10-14 18:53:56 +2000

    added phonebbok behavior definitions

--- src/models/telefonbok.py
+++ src/models/telefonbok.py
@@ -1,2 +1,31 @@
 class Telefonbok:
     _telefonbok = []
+
+    def __init__(self):
+        pass
+
+    # Adds a name with corresponding number to the phonebook
+    # Name != other_names
+    # number != other_numbers
+    def add(self):
+        pass
+
+    # prints a contact to screen based on the name (also alias with the same number)
+    def lookup(self):
+        pass
+
+    # create a new contact with the same number as another contact but a different name
+    def alias(self):
+        pass
+
+    # change the number for a name [including aliases]
+    def change(self):
+        pass
+
+    # Saves the current instance of the phonebook to a filename
+    def save(self):
+        pass
+
+    # fetches a phonebook instance from a file
+    def load(self):
+        pass
